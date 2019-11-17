ddbc024cacda7ea3bc263f166157bc9067e49a81
Kyle Li <kli@brili.co>
2019-11-17 16:00:43 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Even More React
         </a>
       </header>
     </div>
